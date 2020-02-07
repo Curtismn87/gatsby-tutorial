d93f4c3edbad5f3e9c50fe72281793d60af32579
Matt Curtis <Curtismn87@yahoo.com>
2020-02-07 17:00:19 -0500

    gatsy tutorial section 1

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,18 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <div style={{ color: `purple` }}>
+    <h1>Hello Gatsby</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    </div>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
     <Link to="/page-2/">Go to page 2</Link>
+    <br />
+    <Link to="/about/">About</Link>
   </Layout>
 )
 
